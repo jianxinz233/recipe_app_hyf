d7c2284acd91e8554c4f3aa98f56e2007d020a1f
Jianxin <jianxinz233@gmail.com>
2024-12-12 22:53:11 +0100

    app without optional function

---
+++ app/recipeApp.js
@@ -0,0 +1,102 @@
+let recipeObject = {
+    id: 1,
+    title: "Gløgg",
+    picture_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Gl%C3%B6gg_kastrull.JPG/800px-Gl%C3%B6gg_kastrull.JPG",
+    ingredients: [
+      { NAME: "Orange zest", AMOUNT: "0.5" },
+      { NAME: "Water", AMOUNT: "200 ml" },
+      { NAME: "Sugar", AMOUNT: "275 g" },
+      { NAME: "Whole cloves", AMOUNT: "5" },
+      { NAME: "Cinnamon sticks", AMOUNT: "2" },
+      { NAME: "Spice", AMOUNT: undefined },
+      { NAME: "Bottle of red wine", AMOUNT: "1" },
+      { NAME: "Raisins", AMOUNT: "100 g" },
+      { NAME: "Slipped Almonds", AMOUNT: "50 g" },
+    ],
+    description: "Mix everything, heat it, and you are good to go!",
+};
+
+
+
+
+let recipes = []; 
+recipes.push(recipeObject); 
+
+
+
+function addRecipe(event){
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const picture_url = document.getElementById('image-link').value;
+    
+    const ingredients = [];
+    const ingredientNames = document.querySelectorAll("input[name='ingredient_name']");
+    const ingredientAmounts = document.querySelectorAll("input[name='ingredient_amount']");
+    for(let i = 0; i < ingredientNames.length; i++) {
+        const name = ingredientNames[i].value;
+        const amount = ingredientAmounts[i].value;
+        ingredients.push({NAME: name, AMOUNT: amount});
+    }
+
+    const description = document.getElementById('description').value;
+
+    const newRecipe = {
+        id: recipes.length + 1,
+        title,
+        picture_url,
+        ingredients,
+        description
+    };
+
+    recipes.push(newRecipe);
+    showRecipe(newRecipe)
+    
+}
+
+
+
+const addRecipeButton = document.getElementById('add-recipe')
+addRecipeButton.addEventListener('click', addRecipe);
+
+
+
+
+function showRecipe(recipe){
+    const recipeItemElement = document.getElementById('saved-recipes');
+
+    const recipeContainer = document.createElement("div");
+    recipeContainer.classList.add("recipe-item");
+    
+    const recipeTitle = document.createElement("h3");
+    recipeTitle.textContent = recipe.title;
+    recipeContainer.appendChild(recipeTitle);
+
+    const recipeImage = document.createElement("img");
+    recipeImage.src = recipe.picture_url;
+    recipeImage.alt = `${recipe.title}-img`;
+    recipeContainer.appendChild(recipeImage);
+
+    const recipeIngredientsHeader = document.createElement("h4")
+    recipeIngredientsHeader.textContent = "Ingredients:";
+    recipeContainer.appendChild(recipeIngredientsHeader);
+
+    const recipeIngredientsList = document.createElement("ul");
+    for(let ingredient of recipe.ingredients) {
+        const ingredientItem = document.createElement("li");
+        ingredientItem.textContent = `${ingredient.NAME} ${ingredient.AMOUNT}`;
+        recipeIngredientsList.appendChild(ingredientItem);
+    }
+    recipeContainer.appendChild(recipeIngredientsList);
+
+    const recipeDescription = document.createElement("p");
+    recipeDescription.textContent = recipe.description;
+    recipeContainer.appendChild(recipeDescription);
+
+    
+    recipeItemElement.appendChild(recipeContainer)
+}
+
+showRecipe(recipeObject);
+
